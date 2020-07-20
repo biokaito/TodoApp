d9587500dd1a6577a261b4d4f5f2927d909503af
biokaito <bastrieu281@yahoo.com>
2020-07-21 04:13:08 +0700

    Add new item

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
-import TodoItem from './Component/TodoItem'
+import TodoItem from './Component/TodoItem';
+import Tick from './img/tick.svg'
 
 class App extends Component {
   constructor(){
@@ -15,7 +16,7 @@ class App extends Component {
   }
 
   onItemClicked(item){
-    console.log(item)
+    //console.log(item)
     return (event) => {
       const isComplete = item.isComplete;
       const {todoItems} = this.state;
@@ -35,17 +36,38 @@ class App extends Component {
     }
   }
 
+  onKeyUp(event){
+    const text = event.target.value;
+    if(!text || text === ''){
+      return;
+    }
+    text.trim(); //trim() xóa đi các dấu cách ở đầu và cuối
+  }
+
   render(){
     const {todoItems } = this.state;
     if(todoItems.length){
         return (
-          <div className="App">         
+          <div className="App"> 
+          <div className="header"> 
+            <img 
+            src={Tick} 
+            width={32} 
+            height={32}            
+            ></img>
+            <input 
+            type="text" 
+            placeholder="Add a new item"
+            onKeyUp={this.onKeyUp}
+            ></input>
+          </div>       
             {
              todoItems.length && todoItems.map((item,index) =>
               <TodoItem 
-              key={index} 
-              item={item} 
-              onClick={this.onItemClicked(item)} />
+                key={index} 
+                item={item} 
+                onClick={this.onItemClicked(item)} 
+              />
              )
             }
           </div>
